dc4311fa68273f1d4395e76ea0c4f3187e5f02d3
Michael Hernandez <michael.hernandez1988@gmail.com>
2018-10-19 05:27:09 -0400

    fast work

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ if (process.env.NODE_ENV !== 'prod') {
   https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(443)
 })
 } else {
-  http.createServer(app).listen(80);
+  http.createServer(greenlock.middleware(require('redirect-https')())).listen(80);
   https.createServer(greenlock.tlsOptions, app).listen(443);
 }
 
